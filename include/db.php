03ae07399876463e16416b64920cad6a6976804b
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2022-10-02 19:00:34 +0800

    upload boiler plate
    upload boiler plate

---
+++ include/db.php
@@ -0,0 +1,12 @@
+<?php 
+date_default_timezone_set('Asia/Manila');
+$connection = mysqli_connect("localhost", "root", "", "holy");
+
+if(!$connection) {
+         
+    echo "unable to conenct";
+    
+ 
+  }
+?>
+
